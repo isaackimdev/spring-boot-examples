c7a393902785686ff7203408c6e602aec126dcc5
isaac <isaac7263@naver.com>
2023-02-03 01:11:26 +0900

    Thymeleaf Example

---
+++ spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
@@ -0,0 +1,13 @@
+package com.isaac.springbootthymeleaf.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExamController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "/hello.html";
+    }
+}
