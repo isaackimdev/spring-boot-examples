30070eae17dbccf1f2d122c47c3dc7480fd8e5c9
isaac <isaac7263@naver.com>
2023-02-06 00:46:54 +0900

    Thymeleaf - Mvc(Model)

--- spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
+++ spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
@@ -1,6 +1,7 @@
 package com.isaac.springbootthymeleaf.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,12 @@ public class ExamController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello"; // "hello", "/hello", "hello.html"
+    }
+
+    @RequestMapping("/data-test")
+    public String dataTest(Model model) { // Model 선언
+        // Data 세팅
+        model.addAttribute("data1", "대한민국");
+        return "data-test";
     }
 }
