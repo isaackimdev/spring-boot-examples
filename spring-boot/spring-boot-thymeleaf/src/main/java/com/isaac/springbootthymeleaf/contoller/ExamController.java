8e7d885b4489944442b373618fd0561acd5d9a68
isaac <isaac7263@naver.com>
2023-02-07 00:54:46 +0900

    Thymeleaf - ModelAndView test

--- spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
+++ spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
@@ -3,6 +3,7 @@ package com.isaac.springbootthymeleaf.contoller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExamController {
@@ -12,10 +13,24 @@ public class ExamController {
         return "hello"; // "hello", "/hello", "hello.html"
     }
 
+    // Model(data) test
     @RequestMapping("/data-test")
     public String dataTest(Model model) { // Model 선언
         // Data 세팅
         model.addAttribute("data1", "대한민국");
         return "data-test";
+    }
+
+    // ModelAndView test
+    @RequestMapping("/mv-test")
+    public ModelAndView modelAndViewTest(ModelAndView modelAndView) { // Model(Data) + View 지정
+
+        // (1) 데이터 지정
+        // (2) 뷰페이지 지정
+        // (3) return
+        modelAndView.addObject("name", "이순신");
+        modelAndView.addObject("age", 29);
+        modelAndView.setViewName("mv-test");
+        return modelAndView;
     }
 }
