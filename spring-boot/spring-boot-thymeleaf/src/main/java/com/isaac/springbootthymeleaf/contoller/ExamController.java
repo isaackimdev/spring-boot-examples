009a4ac9c95fd74e89b226e230b2e5c165cd36e5
isaac <isaac7263@naver.com>
2023-02-12 21:59:01 +0900

    Thymeleaf + form[get, post]

--- spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
+++ spring-boot/spring-boot-thymeleaf/src/main/java/com/isaac/springbootthymeleaf/contoller/ExamController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -58,9 +60,16 @@ public class ExamController {
     }
 
     // Form
-    @RequestMapping("/form1")
+    @RequestMapping(value = "/form1", method = RequestMethod.GET)
     public String form1(Model model) {
         return "form1";
     }
+
+    @RequestMapping(value = "/form1", method = RequestMethod.POST)
+    public String form2(Model model, @RequestParam("data1") String data1) {
+        model.addAttribute("data1", data1);
+        return "form1";
+    }
+
 
 }
