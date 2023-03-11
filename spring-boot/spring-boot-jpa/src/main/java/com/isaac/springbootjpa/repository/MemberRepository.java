92869ef59ccffa9fa29aa5b8042cb8940d3b242f
isaac <isaac7263@naver.com>
2023-03-11 16:39:01 +0900

    repository equal search

--- spring-boot/spring-boot-jpa/src/main/java/com/isaac/springbootjpa/repository/MemberRepository.java
+++ spring-boot/spring-boot-jpa/src/main/java/com/isaac/springbootjpa/repository/MemberRepository.java
@@ -8,10 +8,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // CrudRepository 또는 JpaRepository 상속.
 // Entity 필요
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+    /**
+     * = search
+     * */
+    Page<Member> findByName(String name, Pageable pageable);
+    Page<Member> findById(String id, Pageable pageable);
+    // like search
+    Page<Member> findByPhone(String phone, Pageable pageable);
+
+
+    /**
+     * like search
+     * */
     // Search Member
     Page<Member> findByNameContaining(String name, Pageable pageable);
     Page<Member> findByIdContaining(String id, Pageable pageable);
-
     // like search
     Page<Member> findByPhoneContaining(String phone, Pageable pageable);
 
