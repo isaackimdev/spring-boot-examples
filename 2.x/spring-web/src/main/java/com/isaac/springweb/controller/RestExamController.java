ca44e5d3c3ffd9f9aff363795fc288d0d3c04f2a
isaac <isaac7263@naver.com>
2023-05-03 16:46:37 +0900

    add: spring-web-example return json

--- 2.x/spring-web/src/main/java/com/isaac/springweb/controller/RestExamController.java
+++ 2.x/spring-web/src/main/java/com/isaac/springweb/controller/RestExamController.java
@@ -3,6 +3,9 @@ package com.isaac.springweb.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class RestExamController {
     @RequestMapping("/hello-rest")
@@ -10,4 +13,12 @@ public class RestExamController {
         return "Hello world!";
     }
 
+    @RequestMapping("/json")
+    public Map<String, Object> json1() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name","isaac");
+        map.put("age", 20);
+        map.put("like alphabet", 'S');
+        return map;
+    }
 }
