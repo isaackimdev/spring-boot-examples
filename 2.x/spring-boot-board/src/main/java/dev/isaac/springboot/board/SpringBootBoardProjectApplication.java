791a61e6f16ee7720d8cb00e41ee45304d1a8079
isaac <isaac7263@naver.com>
2023-05-08 18:16:35 +0900

    add: spring-boot-board project

---
+++ 2.x/spring-boot-board/src/main/java/dev/isaac/springboot/board/SpringBootBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.isaac.springboot.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBoardProjectApplication.class, args);
+	}
+
+}
---
+++ 2.x/spring-boot-board/src/test/java/dev/isaac/springboot/board/SpringBootBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.isaac.springboot.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
