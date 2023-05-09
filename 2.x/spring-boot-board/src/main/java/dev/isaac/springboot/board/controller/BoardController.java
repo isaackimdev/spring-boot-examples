d03400a3df8ec22f8e24c8cacaddb0e819b38e6d
isaac <isaac7263@naver.com>
2023-05-09 18:11:51 +0900

    add: BoardEntity.java, application.yml 설정 추가

--- 2.x/spring-boot-board/src/main/java/dev/isaac/springboot/board/controller/BoardController.java
+++ 2.x/spring-boot-board/src/main/java/dev/isaac/springboot/board/controller/BoardController.java
@@ -1,13 +1,17 @@
 package dev.isaac.springboot.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BoardController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/boardList", method = RequestMethod.GET)
+    public String boardList(Model model) {
+
+        return "boardList";
     }
+
 }
---
+++ 2.x/spring-boot-board/src/main/java/dev/isaac/springboot/board/entities/BoardEntity.java
@@ -0,0 +1,31 @@
+package dev.isaac.springboot.board.entities;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_BOARD")
+public class BoardEntity {
+
+    @Id // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, length = 50)
+    private String author;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    // Variable(class field) 에 @Column Annotation 을 명시하지 않아도
+    // Class 에 @Entity Annotation 이 선언되면 Table 의 Column(Field) 으로 생성한다.
+    private LocalDateTime modifiedDate;
+
+}
