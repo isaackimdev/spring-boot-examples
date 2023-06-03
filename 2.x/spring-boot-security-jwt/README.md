b271202dc0b0b88de218d2ae3fbf153922291f2b
isaac <isaac7263@naver.com>
2023-06-03 10:25:37 +0900

    create spring-boot-security-jwt project

---
+++ 2.x/spring-boot-security-jwt/README.md
@@ -0,0 +1,5 @@
+# Spring boot security & jwt
+spring boot security & jwt 를 사용한 example project 입니다.
+
+project init config
+![start](./image/start.png)
---
+++ 2.x/spring-boot-security-jwt/src/main/java/dev/isaac/springbootsecurityjwt/controller/IndexController.java
@@ -0,0 +1,15 @@
+package dev.isaac.springbootsecurityjwt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello");
+    }
+}
