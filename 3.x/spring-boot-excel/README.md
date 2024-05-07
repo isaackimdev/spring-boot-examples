ea6c904e3b15dd3a3395d430094f5735568f8cc5
isaac <isaac7263@naver.com>
2024-05-08 08:51:42 +0900

    am : excel upload

--- 3.x/spring-boot-excel/README.md
+++ 3.x/spring-boot-excel/README.md
@@ -1,6 +1,9 @@
 # spring boot excel
+spring boot excel upload and read excel data simple example
 
-dependencies
+### dependencies
 - thymeleaf
 - web
 - lombok
+- poi-ooxml
+  - Excel 파일을 읽을 수 있는 library
--- 3.x/spring-boot-excel/src/main/java/com/isaac/excel/controller/ExcelController.java
+++ 3.x/spring-boot-excel/src/main/java/com/isaac/excel/controller/ExcelController.java
@@ -1,12 +1,17 @@
 package com.isaac.excel.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -25,8 +30,31 @@ public class ExcelController {
         Optional<MultipartFile> multipartFile = Optional.of(file);
         // multipartFile.orElseThrow((() -> new RuntimeException("파일 없음")));
         MultipartFile excel = multipartFile.get();
-        String name = excel.getName();
-        log.info("name : " + name);
+        String fileName = excel.getName();
+        long size = excel.getSize();
+        log.info("fileName : " + fileName);
+        log.info("size : " + size);
+
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(excel.getInputStream());
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for(int i = 0; i<sheet.getPhysicalNumberOfRows(); i++) {
+                XSSFRow row = sheet.getRow(i);
+
+                DataFormatter formatter = new DataFormatter();
+
+                String name = formatter.formatCellValue(row.getCell(0));
+                String age = formatter.formatCellValue(row.getCell(1));
+                String phoneNumber = formatter.formatCellValue(row.getCell(2));
+
+                System.out.printf(i+"row : [ %s, %s, %s ]\n", name, age, phoneNumber);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return "/uploadCompletion.html";
     }
 }
